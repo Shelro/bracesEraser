ef3b90d876ba61d2f6221858d4b4afc300e50293
Shelro <inyi13zyy@gmail.com>
2017-05-18 20:39:38 +0800

    mouth detection completed

--- implement/image/braces.cpp
+++ implement/image/braces.cpp
@@ -25,19 +25,22 @@ int main(){
     CascadeClassifier mMouthDetector;
     //载入两个人脸特征分类器文件，可以从opencv的安装目录中找到
     if( mFaceDetector.empty() )
-        mFaceDetector.load( "/Users/zhangyingyi/Documents/重要文件/同济/学习/计算机视觉/FP/image/image/haarcascade_frontalface_alt2.xml" );
+        mFaceDetector.load( "/Users/zhangyingyi/Documents/重要文件/同济/学习/计算机视觉/FP/image/image/haarcascade_frontalface_default.xml" );
     if( mMouthDetector.empty() )
         mMouthDetector.load( "/Users/zhangyingyi/Documents/重要文件/同济/学习/计算机视觉/FP/image/image/haarcascade_mcs_mouth.xml" );
     
     vector< cv::Rect > faceVec;
     Mat mElabImage;//备份frame图像
     img.copyTo( mElabImage );
-    mFaceDetector.detectMultiScale( img, faceVec, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, Size(30, 30));
+    Mat gray;
+    cvtColor(img,gray,CV_BGR2GRAY); //转换成灰度图，因为harr特征从灰度图中提取
+    equalizeHist(gray,gray);  //直方图均衡行
+    mFaceDetector.detectMultiScale( gray, faceVec, 1.1);//, 2, 0 | CV_HAAR_SCALE_IMAGE, Size(30, 30));
     int i, j;
     for(  i=0; i<faceVec.size(); i++ ){
         rectangle( mElabImage, faceVec[i], CV_RGB(255,0,0), 2 );
-        /*
-        Mat face = img( faceVec[i] );
+        Mat face = img(faceVec[i]);
+       
         //检测嘴巴
         vector< cv::Rect > mouthVec;
         Rect halfRect = faceVec[i];
@@ -46,7 +49,11 @@ int main(){
         
         Mat halfFace = img( halfRect );
         
-        mMouthDetector.detectMultiScale( halfFace, mouthVec, 3 );
+        Mat halfgray;
+        cvtColor(halfFace,halfgray,CV_BGR2GRAY); //转换成灰度图，因为harr特征从灰度图中提取
+        equalizeHist(halfgray,halfgray);  //直方图均衡行
+
+        mMouthDetector.detectMultiScale( halfgray, mouthVec, 2.5);
         
         for(  j=0; j<mouthVec.size(); j++ ){
             Rect rect = mouthVec[j];
@@ -54,8 +61,11 @@ int main(){
             rect.y += halfRect.y;
             
             rectangle( mElabImage, rect, CV_RGB(255,255,255), 2 );
-        }*/
+            
+            Mat mouth = halfFace(mouthVec[j]);
+            //imshow("face", mouth);
     }
+}
     
     
     //在窗口中显示图像
